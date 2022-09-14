58b0698a40e3ffd46b78e4b539df6741e5ff5581
Mosesmangwana <Mosesmangwana@outlook.com>
2022-09-14 00:05:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,14 @@ int main(void)
 {
    _putchar('-');
    _putchar('p');
-   _putchar(ร'u');
+   _putchar('u');
    _putchar('t');
-   _putchar(รร'c');
+   _putchar('c');
    _putchar('h');
    _putchar('a');
    _putchar('r');
    _putchar('\n');
 
    return (0);
+
 }
